b0d9400342748368ca9f39ff4c6ac99bea082290
Tom Ch <tangoone@hotmail.no>
2020-10-07 14:27:25 +0200

    update #2

--- gruntfile.js
+++ gruntfile.js
@@ -52,5 +52,5 @@ module.exports = function(grunt) {
 grunt.loadNpmTasks('grunt-contrib-less');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-imagemin');
-grunt.registerTask('default', ['watch']);
+grunt.registerTask('default', ['watch','less','imagemin']);
 };
